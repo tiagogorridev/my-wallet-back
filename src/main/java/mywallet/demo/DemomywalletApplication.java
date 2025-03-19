625c1cd79d2aee69a9f1af463f583d00c1529f74
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-03-18 21:50:17 -0300

    Initial commit

---
+++ src/main/java/mywallet/demo/DemomywalletApplication.java
@@ -0,0 +1,13 @@
+package mywallet.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomywalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemomywalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mywallet/demo/DemomywalletApplicationTests.java
@@ -0,0 +1,13 @@
+package mywallet.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemomywalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
